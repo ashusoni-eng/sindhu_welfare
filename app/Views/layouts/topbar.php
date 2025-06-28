07cc5876476311e7015f608b66639f3e086814da
Ashish Soni <ashish.soni@aeologic.com>
2025-06-28 13:45:36 +0530

    TOpbaar

--- app/Views/layouts/topbar.php
+++ app/Views/layouts/topbar.php
@@ -1,13 +1,13 @@
   <!-- Topbar Start -->
   <div class="container-fluid bg-dark text-white-50 py-2 px-0  topbar">
       <div class="row gx-0 align-items-center">
-          <div class="col-7 px-1 text-start">              
+          <div class="col-8 px-1 text-start">              
               <div class="h-100 d-inline-flex align-items-center me-4">
                   <small class="far fa-user me-2"></small>
                   <small>Coordinated by Mr. Vikas Dawani</small>
               </div>              
           </div>
-          <div class="col-5 px-5 text-end">
+          <div class="col-4 px-5 text-end">
               <div class="h-100 d-inline-flex align-items-center">
                   <a class="text-white-50 ms-4" href=""><i class="fab fa-facebook-f"></i></a>                  
                   <a class="text-white-50 ms-4" href=""><i class="fab fa-instagram"></i></a>
