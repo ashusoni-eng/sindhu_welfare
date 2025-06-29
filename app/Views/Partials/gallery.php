6aa05a1737700b3513356738b854f05699d35692
Ashish Soni <ashish.soni@aeologic.com>
2025-06-29 15:25:47 +0530

    Img

--- app/Views/Partials/gallery.php
+++ app/Views/Partials/gallery.php
@@ -12,7 +12,7 @@
     <?php if (!empty($gallery)): ?>
         <?php foreach ($gallery as $year => $photos): ?>
             <h3 class="my-4 text-center gallery-main-heading"><?= esc($year) ?></h3>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 g-4 mb-5">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 mb-5">
                 <?php foreach ($photos as $photo): ?>
                     <div class="col">
                         <?= view('Partials/gallery_card', ['photo' => $photo]); ?>
